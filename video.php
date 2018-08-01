fdb28a13df570552f15b0e6a1e3d6944ed2fbf49
Zhangsteve <seakiro@msn.com>
2018-08-01 12:07:16 +0200

    Create video.php

---
+++ video.php
@@ -0,0 +1,8 @@
+<?php 
+echo '<div>
+    <video>
+        <source src="http://simpl.info/video/video/chrome.webm" type="video/webm">
+        <source src="http://simpl.info/video/video/chrome.mp4" type="video/mp4">
+      Your browser does not support HTML5 video.
+    </video>
+</div>';
